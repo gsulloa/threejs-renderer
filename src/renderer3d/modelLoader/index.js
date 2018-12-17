2a139c5e13df927dee675b465e13683cc601a62c
Gabriel Ulloa <gsulloa@uc.cl>
2018-12-17 17:25:58 -0300

    downloading from url

--- src/renderer3d/modelLoader/index.js
+++ src/renderer3d/modelLoader/index.js
@@ -1,4 +1,4 @@
-import { Box3, Object3D, Loader } from "three"
+import { Box3, Object3D, LoadingManager } from "three"
 import { OBJLoader, MTLLoader } from "three-obj-mtl-loader"
 import JSZip from "jszip"
 import axios from "axios"
@@ -7,76 +7,78 @@ class ModelLoader {
   constructor({ loading }) {
     this.loading = loading
   }
+
   async load() {
     this.loading.onStart()
+    const files = await this.getFiles()
+    const groupedFiles = files.reduce((all, file) => {
+      if (file.ext !== "mtl" && file.ext !== "obj") {
+        all.textureFiles = { ...all.textureFiles, [file.name]: file.content}
+      } else {
+        all[file.ext] = file.content
+      }
+      return all
+    }, {})
+    const materials = await this.loadMTL(groupedFiles)
+    const object = await this.loadOBJ({ materials, ...groupedFiles })
+    const pivotObject = this.createPivot({ object })
+    this.loading.onLoad()
+    return pivotObject
+  }
+
+  async getFiles() {
     const file = await axios('https://s3.us-east-2.amazonaws.com/idea-files-s3/1507768649217', {
       responseType: "blob",
       onDownloadProgress: this.loading.onProgress,
     })
     const { data: blob } = file
     const { files } = await new JSZip().loadAsync(blob, { type: "blob" })
-    const readContent = (name, file)  =>{
-      const doti = name.lastIndexOf(".")
-      if (doti === -1) throw new Error(`file ${name} without extension`)
-      const ext = name.substring(doti + 1)
-  
-      if (ext === "mtl" || ext === "obj") {
-        return file.async("text").then(content => ({
-          name,
-          blob: new Blob([content], { type: "text/plain" }),
-          ext,
-        }))
-      } else {
-        return file
-          .async("uint8array")
-          .then(content => ({ name, blob: new Blob([content]), ext }))
-      }
-    }
-    const promiseReadeableFiles = await Promise.all(
-      Object.entries(files).map(([name, zipObject]) => readContent(name, zipObject)
+
+    const readedFiles = await Promise.all(
+      Object.entries(files).map(([name, zipObject]) => this.readContent(name, zipObject)
     ))
-    const readableFiles = promiseReadeableFiles.reduce((all, file) => {
-      if (file.ext !== "mtl" && file.ext !== "obj") {
-        all.textureFiles = { ...all.textureFiles, [file.name]: file.blob}
-        all.texturePaths = { ...all.texturePaths, [file.name]: URL.createObjectURL(file.blob)}
-      } else {
-        all[file.ext] = URL.createObjectURL(file.blob)
-      }
-      return all
-    }, {})
 
-    console.log(readableFiles)
-
-    const materials = await this.loadMTL({ mtl: readableFiles.mtl, texturePaths: readableFiles.texturePaths })
-    const object = await this.loadOBJ({ materials })
-
-    this.loading.onLoad()
-    return object
+    return readedFiles
   }
-  loadMTL = (mtl) => new Promise((resolve, reject) => {
-    new MTLLoader()
-      .setPath( 'models/obj/abdomen/' )
-      .load( 'ToraxAbdomen2.mtl', ( materials ) => {
-        // .load(mtl, materials => {
-        materials.preload()
-        resolve(materials)
-      }, () => {}, reject)
-    })
-  loadOBJ = ({ materials }) => new Promise((resolve, reject) => {
-    new OBJLoader()
-      .setMaterials(materials)
-      .setPath('models/obj/abdomen/')
-      .load('ToraxAbdomen2.obj', (object) => {
-        const { max, min } = new Box3().setFromObject(object)
-        const middleY = (max.y - min.y) / 2
-        const middleZ = (max.z - min.z) / 2
-        const pivot = new Object3D();
-        object.position.y = middleY
-        object.position.z = -middleZ
-        pivot.add( object );
-        resolve(pivot)
-      }, () => {}, () => reject("An error occurred loading de model") )
-  })
+  readContent = async (name, file)  =>{
+    const doti = name.lastIndexOf(".")
+    if (doti === -1) throw new Error(`file ${name} without extension`)
+    const ext = name.substring(doti + 1)
+    return {
+      name,
+      ext,
+      content: await file.async(ext === "mtl" || ext === "obj" ? "text" : "uint8array")
+    }
+  }
+  
+  loadMTL = ({ textureFiles, mtl }) => {
+    const loadingManager = new LoadingManager();
+    loadingManager.setURLModifier(url => {
+      const blob = new Blob([textureFiles[url].buffer]);
+      const NewUrl = URL.createObjectURL(blob);
+      return NewUrl
+    });
+    const mtlLoader = new MTLLoader(loadingManager);
+    const materials = mtlLoader.parse(mtl);
+    materials.preload()
+    return materials
+  }
+  loadOBJ = ({ materials, obj }) => {
+    const objLoader = new OBJLoader()
+    objLoader.setMaterials(materials)
+    return objLoader.parse(obj)
+  }
+
+  createPivot({ object }) {
+    const { max, min } = new Box3().setFromObject(object)
+    const middleY = (max.y - min.y) / 2
+    const middleZ = (max.z - min.z) / 2
+    const pivot = new Object3D();
+    object.position.y = middleY
+    object.position.z = -middleZ
+    pivot.add( object );
+    return pivot
+  }
 }
 
 export default ModelLoader
