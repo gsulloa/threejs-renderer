9e0b72bd008ad7eb0c995d78ee164eb1e43b9cbd
Gabriel Ulloa <gsulloa@uc.cl>
2018-12-19 09:37:45 -0300

    hotfix eslint

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,5 @@
-import { devlog, devlogerror } from "./renderer3d/utils/log"
+/* eslint no-console:0 */
+
 // In production, we register a service worker to serve assets from local cache.
 
 // This lets the app load faster on subsequent visits in production, and gives
@@ -40,7 +41,7 @@ export function register(config) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          devlog(
+          console.log(
             "This web app is being served cache-first by a service " +
               "worker. To learn more, visit https://goo.gl/SC7cgQ"
           )
@@ -66,7 +67,7 @@ function registerValidSW(swUrl, config) {
               // the fresh content will have been added to the cache.
               // It's the perfect time to display a "New content is
               // available; please refresh." message in your web app.
-              devlog("New content is available; please refresh.")
+              console.log("New content is available; please refresh.")
 
               // Execute callback
               if (config.onUpdate) {
@@ -76,7 +77,7 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              devlog("Content is cached for offline use.")
+              console.log("Content is cached for offline use.")
 
               // Execute callback
               if (config.onSuccess) {
@@ -88,7 +89,7 @@ function registerValidSW(swUrl, config) {
       }
     })
     .catch(error => {
-      devlogerror("Error during service worker registration:", error)
+      console.error("Error during service worker registration:", error)
     })
 }
 
@@ -113,7 +114,7 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      devlog("No internet connection found. App is running in offline mode.")
+      console.log("No internet connection found. App is running in offline mode.")
     })
 }
 
